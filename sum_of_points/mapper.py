7ad75eabea08c3340e4480767af78ee08dec5a51
Liz <lizconard15@gmail.com>
2018-11-12 12:31:38 -0600

    updated data and mapper.py

---
+++ sum_of_points/mapper.py
@@ -0,0 +1,5 @@
+def mapper():
+	for line in sys.stdin:
+		data = ine.strip().split(",")
+		country, description, designation, points, price, province, region_1, region_2, taster_name, taster_twitter_handle, title, variety, winery = data
+		print "{0}\t{1}".format(country, points)
