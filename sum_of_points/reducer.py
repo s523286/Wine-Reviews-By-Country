10a4f9b511c5fa007e6ab249b94e63834d10bd72
Liz <lizconard15@gmail.com>
2018-11-12 13:08:09 -0600

    edited reducer to test

--- sum_of_points/reducer.py
+++ sum_of_points/reducer.py
@@ -1,19 +1,20 @@
 def reducer():
-	thisKey = ""
-	thisValue = 0
+
+	totalPoints = 0
+	oldKey = None
 
 	for line in sys.stdin:
-		data= line.strip().split("\t")
+		data = line.strip().split("\t")
 
-		if len(data) !=2:
+		if len(data) != 2:
 			continue
-		country, points = data
 
-		if country != thisKey:
-			if thisKey:
-				print"{0}\t{1}\n".format(thisKey, thisValue)
-			thisKey = country	
-			thisValue = 0
-		thisValue += int(points)
-	print"{0}\t{1}\n".format(thisKey, thisValue)
+		thisKey, thisPoints = data
 
+		if oldKey and oldKey != thisKey:
+			print "{0}\t{1}".format(oldKey, totalPoints)
+
+			totalPoints = 0
+
+		oldKey = thisKey
+		salesTotal += float(thisPoints)
