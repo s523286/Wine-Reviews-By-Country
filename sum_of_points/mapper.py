549c8c7639c6fa6685dc06a63cd3ba6f93197ed0
Liz <lizconard15@gmail.com>
2018-11-19 16:27:48 -0600

    Updated mapper.py, sort.py and reducer.py that work in my terminal

--- sum_of_points/mapper.py
+++ sum_of_points/mapper.py
@@ -1,8 +1,11 @@
-#!/usr/bin/python
-
-import sys
-
-for line in sys.stdin:
-	data = line.strip().split("\t")
-	country, description, designation, points, price, province, region_1, region_2, taster_name, taster_twitter_handle, title, variety, winery = data
-	print "{0}\t{1}".format(country, points)
+#Referenced Dr. Case's slides on MapReduce in Python
+#mapper.py will map our data, wineData.txt, to key/value pairs
+f = open("wineData.txt","r")  # open file, read-only raw data
+o = open("o.txt", "w") # open file, write - just our key, value pairs
+for line in f:  
+    data = line.strip().split("\t")
+    if len(data) == 13:
+        country, description, designation, points, price, province, region_1, region_2, taster_name, taster_twitter_handle, title, variety, winery = data
+        o.write("{0}\t{1}\n".format(country, points))
+f.close()
+o.close()
