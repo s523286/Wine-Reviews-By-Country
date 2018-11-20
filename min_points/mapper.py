abf110b6fbb20d5b8a74e954411a281b512817d1
Sierra <S529039@nwmissouri.edu>
2018-11-19 18:23:26 -0600

    Add mapper, sort, and reducer

---
+++ min_points/mapper.py
@@ -0,0 +1,17 @@
+# opens the file to read and write from
+i = open("wineData.txt", "r")
+o = open("o.txt", "w")
+
+# reading the lines in 
+for line in i:
+  # splitting and stripping the data
+  data = line.strip().split("\t")
+  if (len(data) == 13):
+    # titles the fields
+    country, description, designation, points, price, province, region_1, region_2, taster_name, taster_twitter_handle, title, variety, winery = data
+    # writing out the o file with the intermediate key-value pairs
+    o.write(country + "\t" + points + "\n")
+
+# closes the files
+i.close()
+o.close()
---
+++ min_points/reducer.py
@@ -0,0 +1,38 @@
+# opens the file to read and write from 
+s = open("s.txt","r")
+r = open("r.txt", "w")
+
+# setting thisKey and min to 0
+thisKey = ""
+min = 0.0
+
+# It is going to read every line in and strip and split it
+for line in s:
+  data = line.strip().split("\t")
+
+# if the data isnt in key value pairs then it will continue
+  if len(data) != 2:
+    continue
+
+# Setting country and points to data
+  country, points = data
+
+  if country != thisKey:
+    if thisKey:
+      # output the key value pair result
+      r.write(thisKey + '\t' + str(min)+'\n')
+
+    # start over when changing keys
+    thisKey = country 
+    min = points
+  
+  # statment to find the minimum points
+  if points < min:
+    min = points
+
+# output the final entry when done
+r.write(thisKey + '\t' + str(min)+'\n')
+
+# closes the files
+s.close()
+r.close()
